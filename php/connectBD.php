5ef79b1a70fd28af7c371702586cd24652ebb34b
Tomás Matos <58147144+TomasKuki@users.noreply.github.com>
2022-09-23 02:34:06 +0100

    Fix connect BD and start login page

--- php/connectBD.php
+++ php/connectBD.php
@@ -1,8 +1,29 @@
 <?php
-$servername = "94.126.169.155";
-$username = "GiftMeUp";
-$password = "GiftMeUp1234";
-$bd = "kukipt_GiftMeUpAdmin";
+
+//locao connection
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$bd = "kukipt_GiftMeUp";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $bd);
+
+// Check connection
+ if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+//echo "Connected successfully";
+
+
+//bd connection
+
+/** 
+$servername = "kuki.pt";
+$username = "kukipt_GiftMeUpAdmin";
+$password = "GiftMeUpAdmin1234";
+$bd = "kukipt_GiftMeUp";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $bd);
@@ -12,4 +33,4 @@ $conn = new mysqli($servername, $username, $password, $bd);
   die("Connection failed: " . $conn->connect_error);
 }
 echo "Connected successfully";
-?>
+*/?>
